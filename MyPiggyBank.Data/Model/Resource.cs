749e41ab6bfcb4cf9b117ed68d1d4704de181840
Sinity <ezo.dev@gmail.com>
2020-04-26 15:08:52 +0200

    Created db entity model for Resources

---
+++ MyPiggyBank.Data/Model/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Data.Model {
+    class Resource
+    {
+        public Guid ResourceId { get; set; }
+        public string UserEmail { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public string Currency { get; set; }
+    }
+}
