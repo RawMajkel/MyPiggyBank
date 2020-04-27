a6109b9b8c38c4a7ef1d1c323adc2e17748d8892
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 13:29:47 +0200

    Associating User with Resources
    Refference to Resources added in User class

--- MyPiggyBank.Data/Model/User.cs
+++ MyPiggyBank.Data/Model/User.cs
@@ -8,5 +8,6 @@ namespace MyPiggyBank.Data.Model {
         public string Email { get; set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
+        public virtual ICollection<Resource> Resources { get; set; }
     }
 }
