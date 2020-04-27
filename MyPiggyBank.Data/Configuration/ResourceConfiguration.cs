c25b7cd08891e95904b144a1f5f4d518f86bc62a
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 22:28:17 +0200

    Added Configuration for class Resource

---
+++ MyPiggyBank.Data/Configuration/ResourceConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyPiggyBank.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Data.Configuration
+{
+    class ResourceConfiguration
+    {
+        public class UserConfiguration : IEntityTypeConfiguration<Resource>
+        {
+            public void Configure(EntityTypeBuilder<Resource> builder)
+            {
+                builder.HasKey(u => u.Id);
+                builder.Property(u => u.Value)
+                       .IsRequired();
+                builder.Property(u => u.Currency)
+                       .IsRequired();
+                builder.Property(u => u.Name)
+                       .IsRequired();
+            }
+        }
+    }
+}
