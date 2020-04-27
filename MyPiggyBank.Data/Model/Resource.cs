89794630cc0c266c21fc0f2be0ec55ffbf2413dd
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 13:37:34 +0200

    Associating Resource with User
    Refference to User added in Resource class.
    UserEmail change to Email to match field in calss User

--- MyPiggyBank.Data/Model/Resource.cs
+++ MyPiggyBank.Data/Model/Resource.cs
@@ -6,9 +6,10 @@ namespace MyPiggyBank.Data.Model {
     public class Resource
     {
         public Guid Id { get; set; }
-        public string UserEmail { get; set; }
+        public string Email { get; set; }
         public string Name { get; set; }
         public decimal Value { get; set; }
         public string Currency { get; set; }
+        public virtual User User { get; set; }
     }
 }
