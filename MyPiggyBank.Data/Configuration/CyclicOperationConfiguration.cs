929a8ae64ffc708b9b5285893b82db037343a2b4
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 22:30:25 +0200

    Added Configuration for class CyclicOperation

---
+++ MyPiggyBank.Data/Configuration/CyclicOperationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyPiggyBank.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Data.Configuration
+{
+    class CyclicOperationConfiguration : IEntityTypeConfiguration<CyclicOperation>
+    {
+        public void Configure(EntityTypeBuilder<CyclicOperation> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Name)
+                   .IsRequired();
+            builder.Property(u => u.Period)
+                   .IsRequired();
+        }
+    }
+}
