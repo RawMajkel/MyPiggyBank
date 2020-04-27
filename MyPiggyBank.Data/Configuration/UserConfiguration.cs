2db90081cc6107fb48780405e9d06b0a9b023da6
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 21:43:17 +0200

    Added Configuration for class User

---
+++ MyPiggyBank.Data/Configuration/UserConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyPiggyBank.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Data.Configuration
+{
+    class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Email)
+                   .IsRequired();
+            builder.Property(u => u.Username)
+                   .IsRequired();
+        }
+    }
+}
