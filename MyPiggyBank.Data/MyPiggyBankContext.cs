5b4f6e02410e117cc9c9df4dac87655bf170ab55
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 16:38:01 +0200

    Specifying ON DELETE NO ACTION
    Cascade delete disabled in order to add Initial Migration and Update DB

--- MyPiggyBank.Data/MyPiggyBankContext.cs
+++ MyPiggyBank.Data/MyPiggyBankContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using MyPiggyBank.Data.Model;
@@ -11,6 +12,16 @@ namespace MyPiggyBank.Data
         public MyPiggyBankContext(DbContextOptions options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            foreach (var relationship in modelbuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelbuilder);
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Resource> Resources { get; set; }
