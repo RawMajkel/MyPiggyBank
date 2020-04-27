4d9e0e3a4f2224b18fd5133413f80965729cdb9e
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 22:28:54 +0200

    Added Configuration for class OperationCategory

---
+++ MyPiggyBank.Data/Configuration/OperationCategoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyPiggyBank.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Data.Configuration
+{
+    class OperationCategoryConfiguration : IEntityTypeConfiguration<OperationCategory>
+    {
+        public void Configure(EntityTypeBuilder<OperationCategory> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.Name)
+                   .IsRequired();
+        }
+    }
+}
