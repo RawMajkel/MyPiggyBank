26bb926e2fc7313d3cc633e2a89feef424c9f458
Bartek <bartlomiejzon96@gmail.com>
2020-04-27 14:43:19 +0200

    DbSet objects added to Context
    DbSet objects for each Model class added to DBContext class.

--- MyPiggyBank.Data/MyPiggyBankContext.cs
+++ MyPiggyBank.Data/MyPiggyBankContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using MyPiggyBank.Data.Model;
 
 namespace MyPiggyBank.Data
 {
@@ -11,5 +12,10 @@ namespace MyPiggyBank.Data
             : base(options)
         {
         }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Operation> Operations { get; set; }
+        public DbSet<CyclicOperation> CyclicOperations { get; set; }
+        public DbSet<OperationCategory> OperationCategories { get; set; }
     }
 }
