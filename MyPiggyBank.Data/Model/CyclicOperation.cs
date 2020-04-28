4eeec63e6c6f8f700f1197c858d60201cbe0ebfb
Bartek <bartlomiejzon96@gmail.com>
2020-04-28 22:55:09 +0200

    Changes to nullable properties
    EstimatedValue in CyclicOperation can be null.
    UserId in OperationCategory can be null to implement logic of base categories later.

--- MyPiggyBank.Data/Model/CyclicOperation.cs
+++ MyPiggyBank.Data/Model/CyclicOperation.cs
@@ -11,7 +11,7 @@ namespace MyPiggyBank.Data.Model {
         public string Name { get; set; }
         public string Description { get; set; }
         public bool IsIncome { get; set; }
-        public decimal EstimatedValue { get; set; }
+        public decimal? EstimatedValue { get; set; }
         public int Period { get; set; }
         public DateTime NextOccurence { get; set; }
         public virtual Resource Resource { get; set; }
