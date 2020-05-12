a1d3e7be4b7b6969fe4a24912ca9add36f0087fc
Kacper <kacpermatuszek2@gmail.com>
2020-05-12 20:16:30 +0200

    Created user repository.

---
+++ MyPiggyBank.Data/Repositories/Models/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyPiggyBank.Data.Model;
+using MyPiggyBank.Data.Repositories.Base;
+using MyPiggyBank.Data.Repositories.Interfaces;
+
+namespace MyPiggyBank.Data.Repositories.Models
+{
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+        public UserRepository(MyPiggyBankContext context) : base(context)
+        {
+        }
+
+        public async Task Add(User user)
+        {
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User> Get(Guid userId)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        }
+    }
+}
