5014bebd26780c84ae65d8434b9bcb999a530c34
Kacper <kacpermatuszek2@gmail.com>
2020-05-12 22:01:31 +0200

    Initialize communication directory. Created base response.

---
+++ MyPiggyBank.Core/Communication/Response/Base/BaseResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Core.Communication.Response.Base
+{
+    public abstract class BaseResponse
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
