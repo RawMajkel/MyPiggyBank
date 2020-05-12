f8ad23186e4a98bce633a945eb6a937ba7a84592
Kacper <kacpermatuszek2@gmail.com>
2020-05-12 19:41:24 +0200

    Created interface for user repository.

---
+++ MyPiggyBank.Data/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MyPiggyBank.Data.Model;
+
+namespace MyPiggyBank.Data.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task Add(User user);
+        public Task<User> Get(Guid userId);
+    }
+}
