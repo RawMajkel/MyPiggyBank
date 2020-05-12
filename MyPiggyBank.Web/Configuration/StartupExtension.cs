815da7becdf7acf79a0b507796734f4a9a15120a
Kacper <kacpermatuszek2@gmail.com>
2020-05-12 21:19:43 +0200

    Startup configuration.

---
+++ MyPiggyBank.Web/Configuration/StartupExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyPiggyBank.Data;
+using MyPiggyBank.Data.Repositories.Interfaces;
+using MyPiggyBank.Data.Repositories.Models;
+
+namespace MyPiggyBank.Web.Configuration
+{
+    public static class StartupExtension
+    {
+        public static void Configure(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<MyPiggyBankContext>(opt =>
+                        opt.UseLazyLoadingProxies()
+                            .UseSqlServer(configuration.GetConnectionString(nameof(MyPiggyBankContext))))
+                    .AddControllers();
+        }
+        private static IServiceCollection ConfigureRepositories(this IServiceCollection service)
+        {
+            return service.AddTransient<IUserRepository, UserRepository>();
+        }
+    }
+}
--- MyPiggyBank.Web/Startup.cs
+++ MyPiggyBank.Web/Startup.cs
@@ -1,10 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using MyPiggyBank.Data;
+using MyPiggyBank.Web.Configuration;
 
 namespace MyPiggyBank.Web
 {
@@ -20,10 +19,7 @@ namespace MyPiggyBank.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<MyPiggyBankContext>(opt =>
-                    opt.UseLazyLoadingProxies()
-                        .UseSqlServer(Configuration.GetConnectionString(nameof(MyPiggyBankContext))))
-                .AddControllers();
+            services.Configure(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
