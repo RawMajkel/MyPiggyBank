62e906e4c417c701514822873a9eb14618aa189c
Kacper <kacpermatuszek2@gmail.com>
2020-05-13 21:17:58 +0200

    Account Profile Test.

---
+++ MyPiggyBank.Test/Account/AccountProfileTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using MyPiggyBank.Core.Communication.Account.Mappings;
+using MyPiggyBank.Core.Communication.Account.Requests;
+using MyPiggyBank.Data.Model;
+using Xunit;
+
+namespace MyPiggyBank.Test.Account
+{
+    public class AccountProfileTest
+    {
+        private readonly MapperConfiguration _mapperConfiguration;
+
+        public AccountProfileTest()
+        {
+            _mapperConfiguration = new MapperConfiguration(c =>
+            {
+                c.AddProfile<AccountProfile>();
+            });
+        }
+
+        [Fact]
+        public void Profile_ShouldBeValid()
+        {
+            _mapperConfiguration.AssertConfigurationIsValid();
+        }
+    }
+}
