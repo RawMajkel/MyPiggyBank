10654c2599ea6e51ab141ba050fe92955cdcc565
Kacper <kacpermatuszek2@gmail.com>
2020-05-13 21:02:04 +0200

    Created Account profile. Configuration automapper.

---
+++ MyPiggyBank.Core/Communication/Account/Mappings/AccountProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MyPiggyBank.Core.Communication.Account.Requests;
+using MyPiggyBank.Data.Model;
+
+namespace MyPiggyBank.Core.Communication.Account.Mappings
+{
+    public class AccountProfile : Profile
+    {
+        public AccountProfile()
+        {
+            CreateMap<RegisterRequest, User>()
+                .ForMember(d => d.Username, opt => opt.MapFrom(s => s.UserName))
+                .ForMember(d => d.Id, opt => opt.Ignore())
+                .ForMember(d => d.PasswordHash, opt => opt.Ignore())
+                .ForMember(d => d.OperationCategories, opt => opt.Ignore())
+                .ForMember(d => d.Resources, opt => opt.Ignore());
+        }
+    }
+}
--- MyPiggyBank.Web/Configuration/StartupExtension.cs
+++ MyPiggyBank.Web/Configuration/StartupExtension.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyPiggyBank.Core.Communication.Account.Mappings;
 using MyPiggyBank.Data;
 using MyPiggyBank.Data.Repositories.Interfaces;
 using MyPiggyBank.Data.Repositories.Models;
@@ -14,11 +16,22 @@ namespace MyPiggyBank.Web.Configuration
             service.AddDbContext<MyPiggyBankContext>(opt =>
                         opt.UseLazyLoadingProxies()
                             .UseSqlServer(configuration.GetConnectionString(nameof(MyPiggyBankContext))))
+                    .RegisterProfiles()
                     .AddControllers();
         }
         private static IServiceCollection ConfigureRepositories(this IServiceCollection service)
         {
             return service.AddTransient<IUserRepository, UserRepository>();
+        }
+
+        private static IServiceCollection RegisterProfiles(this IServiceCollection service)
+        {
+            var mappingConf = new MapperConfiguration(mc => { mc.AddProfile<AccountProfile>(); });
+            IMapper mapper = mappingConf.CreateMapper();
+
+            service.AddSingleton(mapper);
+
+            return service;
         }
     }
 }
