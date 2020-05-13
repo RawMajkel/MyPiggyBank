f20e9d9dc28e33c303515b0cf66f461d981f5c1d
Kacper <kacpermatuszek2@gmail.com>
2020-05-13 19:48:10 +0200

    Initial AccountService interface.

---
+++ MyPiggyBank.Core/Services/Account/Interface/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MyPiggyBank.Core.Services.Account.Interface
+{
+    public interface IAccountService
+    {
+        Task SaveAccount(string userName, string password);
+    }
+}
