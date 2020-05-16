feeb5a2485c372d64af15a12f82ccc5c1d219f12
Kacper <kacpermatuszek2@gmail.com>
2020-05-16 18:28:59 +0200

    Validation for login input.

---
+++ MyPiggyBank.Core/Communication/Account/Validators/LoginRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using MyPiggyBank.Core.Communication.Account.Requests;
+
+namespace MyPiggyBank.Core.Communication.Account.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(l => l.Password)
+                .NotEmpty();
+            RuleFor(l => l.Email)
+                .NotEmpty()
+                .EmailAddress();
+        }
+    }
+}
