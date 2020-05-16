4e196281d4b60f9dea767bfb23393b83fa74fa2d
Kacper <kacpermatuszek2@gmail.com>
2020-05-16 14:02:19 +0200

    Runtime database.

---
+++ MyPiggyBank.Integration.Test/DbHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using MyPiggyBank.Data;
+
+namespace MyPiggyBank.Integration.Test
+{
+    public static class DbHelper
+    {
+        public static MyPiggyBankContext CreateDbInRuntimeMemory() 
+        {
+            var dbOptions = new DbContextOptionsBuilder<MyPiggyBankContext>()
+                .UseInMemoryDatabase(databaseName: "TestIntegrationDB")
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            return new MyPiggyBankContext(dbOptions);
+        }
+    }
+}
