b7f9eaee7569af584ff2c79b103396afb5665389
Kacper <kacpermatuszek2@gmail.com>
2020-05-16 14:34:13 +0200

    Created register method in controller.

--- MyPiggyBank.Web/Controllers/AccountController.cs
+++ MyPiggyBank.Web/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyPiggyBank.Core.Communication.Account.Requests;
+using MyPiggyBank.Core.Services.Account.Interface;
 
 namespace MyPiggyBank.Web.Controllers
 {
@@ -11,5 +13,25 @@ namespace MyPiggyBank.Web.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest input)
+        {
+            try
+            {
+                await _accountService.SaveAccount(input);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
