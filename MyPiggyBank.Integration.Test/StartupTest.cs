ad9af0fe3fc37d3f64a581617d59a6e9ec89e6b6
Kacper <kacpermatuszek2@gmail.com>
2020-05-17 16:06:14 +0200

    Created Startup and client for tests.

---
+++ MyPiggyBank.Integration.Test/StartupTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyPiggyBank.Data;
+using MyPiggyBank.Web.Configuration;
+
+namespace MyPiggyBank.Integration.Test
+{
+    public class StartupTest
+    {
+        public IConfiguration Configuration { get; }
+        public StartupTest(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionStringPiggyTest = Configuration.GetConnectionString(nameof(MyPiggyBankContext));
+
+            Action action = !string.IsNullOrEmpty(connectionStringPiggyTest)
+                ? (Action) (() =>
+                {
+                    services.ConfigureMyPiggyBankContext(Configuration);
+                })
+                : () =>
+                {
+                    var dbRuntime = DbHelper.CreateDbInRuntimeMemory();
+                    services.AddTransient<MyPiggyBankContext>(_ => dbRuntime);
+                };
+
+            action.Invoke();
+            services.Configure(Configuration);
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            StartupExtension.ConfigureApp(app, env);
+        }
+    }
+}
---
+++ MyPiggyBank.Integration.Test/TestClient.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+
+namespace MyPiggyBank.Integration.Test
+{
+    public class TestClient
+    {
+        private static string _localhost = "http://localhost:5001";
+        private static TestServer _server;
+
+        public static HttpClient CreateHost()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Test.json")
+                .Build();
+
+            var webHostBuilder = new WebHostBuilder()
+                .UseConfiguration(configurationBuilder)
+                .UseStartup<StartupTest>()
+                .UseKestrel()
+                .UseUrls(_localhost);
+                 
+            _server = new TestServer(webHostBuilder);
+
+            var client = _server.CreateClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
