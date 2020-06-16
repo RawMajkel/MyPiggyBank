7d6334e1d0b3cb4978665cc0e7ad593747c47ffd
Bartek <bartlomiejzon96@gmail.com>
2020-06-16 23:24:48 +0200

    Extracted interface from OperationCategoryService

---
+++ MyPiggyBank.Core/Service/Interface/IOperationCategoriesService.cs
@@ -0,0 +1,16 @@
+﻿using MyPiggyBank.Core.Protocol.Query;
+using MyPiggyBank.Data.Model;
+using MyPiggyBank.Data.Util;
+using System;
+using System.Threading.Tasks;
+
+namespace MyPiggyBank.Core.Service.Implementation
+{
+    public interface IOperationCategoriesService
+    {
+        Task DeleteOperationCategory(Guid operationCategoryId);
+        Task<OperationCategory> Get(Guid id);
+        PagedList<OperationCategory> GetOperationCategories(OperationCategoriesQuery query);
+        Task SaveOperationCategory(OperationCategory source);
+    }
+}
