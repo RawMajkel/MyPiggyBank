1133212dfc5ca339b33f2fc7609c4b90755cf0c9
Bartek <bartlomiejzon96@gmail.com>
2020-06-16 22:16:29 +0200

    Interface for OperationCategoryRepository
    Created

---
+++ MyPiggyBank.Data/Repository/Implementation/IOperationsCategoryRepository.cs
@@ -0,0 +1,6 @@
+﻿using MyPiggyBank.Data.Model;
+
+namespace MyPiggyBank.Data.Repository.Implementation
+{
+    public interface IOperationsCategoryRepository : IBaseRepository<OperationCategory> { }
+}
