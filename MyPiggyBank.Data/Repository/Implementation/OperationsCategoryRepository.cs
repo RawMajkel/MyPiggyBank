a6cc3d5cff38e82c2211658a05f411d3b8960d82
Bartek <bartlomiejzon96@gmail.com>
2020-06-16 22:14:37 +0200

    OperationsCategoryRepository
    Created

---
+++ MyPiggyBank.Data/Repository/Implementation/OperationsCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using MyPiggyBank.Data.Model;
+
+namespace MyPiggyBank.Data.Repository.Implementation
+{
+    class OperationsCategoryRepository : BaseRepository<OperationCategory>, IOperationsCategoryRepository
+    {
+        public OperationsCategoryRepository(MyPiggyBankContext context) : base(context) { }
+    }
+}
