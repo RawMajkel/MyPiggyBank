1e40a0f2def7e9232e9d3c13dee3467d699c99c3
Bartek <bartlomiejzon96@gmail.com>
2020-06-18 18:05:18 +0200

    Extracted interface from CyclicOperationService

---
+++ MyPiggyBank.Core/Service/Interface/ICyclicOperationsService.cs
@@ -0,0 +1,16 @@
+﻿using MyPiggyBank.Core.Protocol.Query;
+using MyPiggyBank.Data.Model;
+using MyPiggyBank.Data.Util;
+using System;
+using System.Threading.Tasks;
+
+namespace MyPiggyBank.Core.Service
+{
+    public interface ICyclicOperationsService
+    {
+        Task DeleteCyclicOperation(Guid cyclicOperationId);
+        Task<CyclicOperation> Get(Guid id);
+        PagedList<CyclicOperation> GetCyclicOperations(CyclicOperationsQuery query);
+        Task SaveCyclicOperation(CyclicOperation source);
+    }
+}
