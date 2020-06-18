529e7ff8571cbe89d04676085a273c5b02527a4c
Bartek <bartlomiejzon96@gmail.com>
2020-06-18 18:19:29 +0200

    QueryValidator for OperationCategories added

---
+++ MyPiggyBank.Core/Protocol/Query/Validators/OperationCategoriesValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace MyPiggyBank.Core.Protocol.Query.Validators
+{
+    public class OperationCategoriesValidator : AbstractValidator<OperationCategoriesQuery>
+    {
+        public OperationCategoriesValidator()
+        {
+            RuleFor(q => q.Name)
+                .NotEmpty()
+                .Length(1,20)
+                .WithMessage("Length of Category name should be between 1 and 20.");
+        }
+    }
+}
