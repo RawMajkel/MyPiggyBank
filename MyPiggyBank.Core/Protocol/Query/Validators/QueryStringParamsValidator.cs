594cee858f9597bb53eae0362b27f31050c4391e
Sinity <ezo.dev@gmail.com>
2020-06-19 16:50:48 +0200

    Added validation of page number to querystring validator

--- MyPiggyBank.Core/Protocol/Query/Validators/QueryStringParamsValidator.cs
+++ MyPiggyBank.Core/Protocol/Query/Validators/QueryStringParamsValidator.cs
@@ -7,8 +7,14 @@ namespace MyPiggyBank.Core.Protocol.Query.Validators
         public QueryStringParamsValidator()
         {
             RuleFor(q => q.Limit)
-                .NotEmpty().InclusiveBetween(1, 100)
+                .NotEmpty()
+                .InclusiveBetween(1, 100)
                 .WithMessage("Collection size limit should be set between 1 and 100.");
+
+            RuleFor(q => q.Page)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Page number can't be negative or 0.");
         }
     }
 }
