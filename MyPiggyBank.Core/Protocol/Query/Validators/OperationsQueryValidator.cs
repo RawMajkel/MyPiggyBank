56f778db2e34deadd66283f84327e15a838006e0
Sinity <ezo.dev@gmail.com>
2020-06-19 17:09:09 +0200

    Created validation rules for operations query

--- MyPiggyBank.Core/Protocol/Query/Validators/OperationsQueryValidator.cs
+++ MyPiggyBank.Core/Protocol/Query/Validators/OperationsQueryValidator.cs
@@ -6,8 +6,18 @@ namespace MyPiggyBank.Core.Protocol.Query.Validators
     {
         public OperationsQueryValidator()
         {
-            // TODO what about rules for base class (QueryStringParams)?
-            // TODO validation rules
+            RuleFor(q => q.Name)
+                .NotEmpty()
+                .Length(1, 200)
+                .WithMessage("Length of the operation name should be between 1 and 200.");
+
+            RuleFor(q => q)
+                .Must(res => ((res.MinValue == null || res.MaxValue == null) || res.MinValue <= res.MaxValue))
+                .WithMessage("Min value can't be higher than max value");
+
+            RuleFor(q => q)
+                .Must(res => ((res.MinOccuredAt == null || res.MaxOccuredAt == null) || res.MinOccuredAt <= res.MaxOccuredAt))
+                .WithMessage("Earliest date occured at can't be later than latest occured date at.");
         }
     }
 }
