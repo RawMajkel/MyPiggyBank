fedd1cfc9b45a576f31a92939cb30d04f5624e7d
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 14:48:34 +0200

    CyclicOperationResponse added

---
+++ MyPiggyBank.Core/Protocol/CyclicOperation/Responses/CyclicOperationResponse.cs
@@ -0,0 +1,24 @@
+﻿using MyPiggyBank.Core.Protocol.Base;
+using System;
+
+namespace MyPiggyBank.Core.Protocol.CyclicOperation.Responses
+{
+    public class CyclicOperationResponse : QueryStringParams
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? EstimatedValue { get; set; }
+        public decimal? MinEstimatedValue { get; set; }
+        public decimal? MaxEstimatedValue { get; set; }
+        public Guid ResourceId { get; set; }
+        public Guid OperationCategoryId { get; set; }
+        public bool? IsIncome { get; set; }
+        public int? Period { get; set; }
+        public int? MinPeriod { get; set; }
+        public int? MaxPeriod { get; set; }
+        public DateTime NextOccurence { get; set; }
+        public DateTime? MinNextOccurence { get; set; }
+        public DateTime? MaxNextOccurence { get; set; }
+    }
+}
