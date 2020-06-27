3672e59b9c6e1b1ceb53a9e302b0ab32b18f9423
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:30:42 +0200

    CyclicOperationsValidators cleanup
    Only Validator for Request required - updated

--- MyPiggyBank.Core/Protocol/CyclicOperation/Validators/CyclicOperationRequestValidator.cs
+++ MyPiggyBank.Core/Protocol/CyclicOperation/Validators/CyclicOperationRequestValidator.cs
@@ -6,10 +6,16 @@ using System.Text;
 
 namespace MyPiggyBank.Core.Protocol.CyclicOperation.Validators
 {
-    public class CyclicOperationRequestValidator : AbstractValidator<CyclicOperationRequest>
+    public class CyclicOperationRequestValidator : AbstractValidator<CyclicOperationSaveRequest>
     {
         public CyclicOperationRequestValidator()
         {
+            RuleFor(r => r.ResourceId)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_Resource_Empty_Error);
+            RuleFor(r => r.OperationCategoryId)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_OperationCategory_Empty_Error);
             RuleFor(r => r.Name)
                 .NotEmpty()
                 .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_Name_Empty_Error)
