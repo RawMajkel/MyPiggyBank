dc3fcade13f3f409ea092b79bad918d47d3336e8
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:48:34 +0200

    OperationCategoriesProfile Test added
    Tests mapper and if creation of base Id

---
+++ MyPiggyBank.Test/OperationCategories/OperationCategoriesProfileTestcs.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MyPiggyBank.Core.Protocol.OperationCategories.Mapping;
+using MyPiggyBank.Core.Protocol.OperationCategories.Requests;
+using System;
+using Xunit;
+
+namespace MyPiggyBank.Test.OperationCategories
+{
+    public class OperationCategoriesProfileTest
+    {
+        private readonly MapperConfiguration _mapperConfiguration;
+
+        public OperationCategoriesProfileTest()
+        {
+            _mapperConfiguration = new MapperConfiguration(c =>
+            {
+                c.AddProfile<OperationCategoriesProfile>();
+            });
+        }
+
+        [Fact]
+        public void Profile_ShouldBeValid()
+        {
+            _mapperConfiguration.AssertConfigurationIsValid();
+        }
+        [Fact]
+        public void OperationIdMapping()
+        {
+            OperationCategoriesSaveRequest test = new OperationCategoriesSaveRequest { Id = Guid.NewGuid() };
+            var mapper = _mapperConfiguration.CreateMapper();
+            var entity = mapper.Map<Data.Model.OperationCategory>(test);
+            Assert.True(entity.Id == test.Id);
+        }
+    }
+}
