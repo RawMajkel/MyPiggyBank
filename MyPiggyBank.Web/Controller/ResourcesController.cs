1654fa72f4597a20f110247d0ae5c1528751ca44
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:50:39 +0200

    ResourcesController update
    Update to mach latest service

--- MyPiggyBank.Web/Controller/ResourcesController.cs
+++ MyPiggyBank.Web/Controller/ResourcesController.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MyPiggyBank.Core.Protocol.Resource;
 using MyPiggyBank.Core.Service;
 using MyPiggyBank.Data.Model;
 using Newtonsoft.Json;
+using MyPiggyBank.Core.Protocol.Resource.Requests;
 
 namespace MyPiggyBank.Web.Controllers {
     [Route("api/v1/[controller]")]
@@ -18,7 +18,7 @@ namespace MyPiggyBank.Web.Controllers {
         }
 
         [HttpGet]
-        public IActionResult Get([FromQuery] ResourcesQuery query)
+        public IActionResult Get([FromQuery] ResourceGetRequest query)
         {
             var resources = _resourcesService.GetResources(query);
             Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(resources.PagingData()));
@@ -26,30 +26,60 @@ namespace MyPiggyBank.Web.Controllers {
         }
 
         [HttpPost]
-        public void Post([FromBody] Resource res)
-            => _resourcesService.SaveResource(res);
+        public async Task<IActionResult> Post([FromBody] ResourceSaveRequest res)
+        {
+            try
+            {
+                await _resourcesService.SaveResource(res);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> Get(Guid id)
         {
             try
             {
-                await _resourcesService.Get(id);
-                return Ok();
-            } catch (Exception e)
+                var result = await _resourcesService.Get(id);
+                return Ok(result);
+            } 
+            catch (Exception e)
             {
-
                 return BadRequest(e.Message);
             }
         }
 
         [HttpPut("{id:guid}")]
-        public void Put(Guid id, [FromBody] Resource res)
-            => _resourcesService.SaveResource(res);
+        public async Task<IActionResult> Put(Guid id, [FromBody] ResourceSaveRequest res)
+        {
+            try
+            {
+                await _resourcesService.SaveResource(res);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
 
 
         [HttpDelete("{id:guid}")]
-        public void Delete(Guid id)
-            => _resourcesService.DeleteResource(id);
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _resourcesService.DeleteResource(id);
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
