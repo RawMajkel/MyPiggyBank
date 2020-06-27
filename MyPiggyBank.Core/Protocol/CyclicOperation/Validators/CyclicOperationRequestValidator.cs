5557a19547f7460d59e5c9112cfd66d00bbf77b1
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 14:49:18 +0200

    Validators with resources added

---
+++ MyPiggyBank.Core/Protocol/CyclicOperation/Validators/CyclicOperationRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MyPiggyBank.Core.Protocol.CyclicOperation.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Core.Protocol.CyclicOperation.Validators
+{
+    public class CyclicOperationRequestValidator : AbstractValidator<CyclicOperationRequest>
+    {
+        public CyclicOperationRequestValidator()
+        {
+            RuleFor(r => r.Name)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_Name_Empty_Error)
+                .MinimumLength(4)
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_Name_Length_Error);
+            RuleFor(r => r.IsIncome)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_IsIncome_Empty_Error);
+            RuleFor(r => r.Period)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_Period_Empty_Error);
+            RuleFor(r => r.NextOccurence)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_NextOccurence_Empty_Error);
+        }
+    }
+}
---
+++ MyPiggyBank.Core/Protocol/CyclicOperation/Validators/CyclicOperationResponseValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MyPiggyBank.Core.Protocol.CyclicOperation.Responses;
+
+namespace MyPiggyBank.Core.Protocol.CyclicOperation.Validators
+{
+    public class CyclicOperationResponseValidator : AbstractValidator<CyclicOperationResponse>
+    {
+        public CyclicOperationResponseValidator()
+        {
+            RuleFor(r => r.ResourceId)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_Resource_Empty_Error);
+            RuleFor(r => r.OperationCategoryId)
+                .NotEmpty()
+                .WithMessage(CyclicOperationResources.CyclicOperationRequestValidator_OperationCategory_Empty_Error);
+        }
+    }
+}
