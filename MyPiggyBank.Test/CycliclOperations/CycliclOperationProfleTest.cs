918c19ca1ae3bb23e7f100baa5f9f6982a4cf440
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 14:50:56 +0200

    Test for CyclicOperations MapperConfiguration

---
+++ MyPiggyBank.Test/CycliclOperations/CycliclOperationProfleTest.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MyPiggyBank.Core.Protocol.CyclicOperation.Mapping;
+using Xunit;
+
+namespace MyPiggyBank.Test.CycliclOperations
+{
+    public class CycliclOperationProfleTest
+    {
+        private readonly MapperConfiguration _mapperConfiguration;
+
+        public CycliclOperationProfleTest()
+        {
+            _mapperConfiguration = new MapperConfiguration(c =>
+            {
+                c.AddProfile<CyclicOperationRequestProfile>();
+                c.AddProfile<CyclicOperationResponseProfile>();
+            });
+        }
+
+        [Fact]
+        public void Profile_ShouldBeValid()
+        {
+            _mapperConfiguration.AssertConfigurationIsValid();
+        }
+    }
+}
