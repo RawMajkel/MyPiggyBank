18411dc74d57aeb08b08721a1b50fb7e23c00175
Michał Droździk <m.drozdzik97@gmail.com>
2020-06-27 12:25:02 +0200

    Added Footer component

---
+++ FrontEnd/react-app/src/modules/Global/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    // <nav className="footernav position-fixed w-100">
+    //     <div className="container">
+    //         <ul className="footernav__ul d-flex flex-nowrap align-items-center justify-content-around">
+    //             <li className="footernav__item footernav__item--active">
+    //                 <a href="/" className="footernav__a d-flex flex-column align-items-center">
+    //                     <i className="footernav__icon fas fa-home"></i>
+    //                     <span className="footernav__label">Pulpit</span>
+    //                 </a>
+    //             </li>
+    //             <li className="footernav__item">
+    //                 <a href="/resources" className="footernav__a d-flex flex-column align-items-center">
+    //                     <i className="footernav__icon fas fa-credit-card"></i>
+    //                     <span className="footernav__label">Rachunki</span>
+    //                 </a>
+    //             </li>
+    //             <li className="footernav__item">
+    //                 <a href="/finances" className="footernav__a d-flex flex-column align-items-center">
+    //                     <i className="footernav__icon fas fa-wallet"></i>
+    //                     <span className="footernav__label">Finanse</span>
+    //                 </a>
+    //             </li>
+    //             <li className="footernav__item">
+    //                 <a href="/transactions" className="footernav__a d-flex flex-column align-items-center">
+    //                     <i className="footernav__icon fas fa-exchange-alt"></i>
+    //                     <span className="footernav__label">Transakcje</span>
+    //                 </a>
+    //             </li>
+    //         </ul>
+    //     </div>
+    // </nav>
+  );
+}
+
+export default Footer;
