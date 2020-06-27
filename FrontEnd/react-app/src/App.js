43eb962c692f090cac464fa3641eaf10befb80f5
Michał Droździk <m.drozdzik97@gmail.com>
2020-06-27 12:22:57 +0200

    Updated App.js and added routings

--- FrontEnd/react-app/src/App.js
+++ FrontEnd/react-app/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useRoutes} from 'hookrouter';
+import Home from './modules/Home/Home';
+import Footer from './modules/Global/Footer';
+import Header from './modules/Global/Header';
+
+const routes = {
+  '/': () => <Home />
+  // '/resources': () => ,
+  // '/finances': () => ,
+  // '/transactions': () => ,
+  // '/account': () => ,
+  // '/categories': () => ,
+  // '/rate': () => ,
+  // '/settings': () =>
+};
 
 function App() {
+
+  const routeResult = useRoutes(routes);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {routeResult}
+      <Footer />
     </div>
   );
 }
