91a24342f3ac00ae8c7c944f904c3469c7ce7298
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 14:49:38 +0200

    CyclicOperation Profiles Added

---
+++ MyPiggyBank.Core/Protocol/CyclicOperation/Mapping/CyclicOperationRequestProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MyPiggyBank.Core.Protocol.CyclicOperation.Requests;
+using MyPiggyBank.Data.Model;
+
+namespace MyPiggyBank.Core.Protocol.CyclicOperation.Mapping
+{
+    public class CyclicOperationRequestProfile : Profile
+    {
+        public CyclicOperationRequestProfile()
+        {
+            CreateMap<CyclicOperationRequest, Data.Model.CyclicOperation>()
+                .ForMember(d => d.OperationCategory, opt => opt.Ignore())
+                .ForMember(d => d.Resource, opt => opt.Ignore())
+                .ReverseMap();
+        }
+    }
+}
---
+++ MyPiggyBank.Core/Protocol/CyclicOperation/Mapping/CyclicOperationResponseProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MyPiggyBank.Core.Protocol.CyclicOperation.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Core.Protocol.CyclicOperation.Mapping
+{
+    public class CyclicOperationResponseProfile : Profile
+    {
+        public CyclicOperationResponseProfile()
+        {
+            CreateMap<CyclicOperationResponse, Data.Model.CyclicOperation>()
+                .ForMember(d => d.OperationCategory, opt => opt.Ignore())
+                .ForMember(d => d.Resource, opt => opt.Ignore())
+                .ReverseMap();
+        }
+    }
+}
