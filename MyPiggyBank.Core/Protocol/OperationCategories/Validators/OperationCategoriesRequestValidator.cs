dcede84859fedbe5f9a0cb473c32216128fb4a75
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:38:12 +0200

    Validator for OperationCategoriesRequest Added

---
+++ MyPiggyBank.Core/Protocol/OperationCategories/Validators/OperationCategoriesRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MyPiggyBank.Core.Protocol.OperationCategories.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Core.Protocol.OperationCategories.Validators
+{
+    public class OperationCategoriesRequestValidator : AbstractValidator<OperationCategoriesSaveRequest>
+    {
+        public OperationCategoriesRequestValidator()
+        {
+            RuleFor(r => r.UserId)
+                   .NotEmpty()
+                   .WithMessage(OperationCategoriesResources.OperationCategoriesRequestValidator_UserId_Empty_Error);
+
+            RuleFor(r => r.Name)
+                .NotEmpty()
+                .WithMessage(OperationCategoriesResources.OperationCategoriesRequestValidator_Name_Empty_Error)
+                .MinimumLength(4)
+                .WithMessage(OperationCategoriesResources.OperationCategoriesRequestValidator_Name_Length_Error);
+        }
+    }
+}
