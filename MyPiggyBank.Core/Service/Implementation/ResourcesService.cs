dd6309ce5eb0923f9fe44a8f3314aed23fc7c1a6
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:45:27 +0200

    Resources Service and Interface updated to match changes made.

--- MyPiggyBank.Core/Service/Implementation/ResourcesService.cs
+++ MyPiggyBank.Core/Service/Implementation/ResourcesService.cs
@@ -6,6 +6,8 @@ using MyPiggyBank.Data.Util;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using MyPiggyBank.Core.Protocol.Resource.Requests;
+using MyPiggyBank.Core.Protocol.Resource.Responses;
 
 namespace MyPiggyBank.Core.Service {
     public class ResourcesService : IResourcesService
@@ -19,22 +21,25 @@ namespace MyPiggyBank.Core.Service {
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
-        public PagedList<ResourceInfo> GetResources(ResourcesQuery query)
-             => PagedList<ResourceInfo>.ToPagedList(_repository.GetAll()
-                .Where(r => query.User == Guid.Empty || r.UserId == query.User)
+        public PagedList<ResourceResponse> GetResources(ResourceGetRequest query)
+             => PagedList<ResourceResponse>.ToPagedList(_repository.GetAll()
+                .Where(r => query.UserId == Guid.Empty || r.UserId == query.UserId)
                 .Where(r => r.Name == (query.Name ?? r.Name))
                 .Where(r => r.Currency == (query.Currency ?? r.Currency))
                 .Where(r => r.Value >= (query.MinValue ?? r.Value))
-                  .Where(r => r.Value <= (query.MaxValue ?? r.Value))
+                .Where(r => r.Value <= (query.MaxValue ?? r.Value))
                 .OrderBy(on => on.Name)
-                .Select(r => _mapper.Map<ResourceInfo>(r)),
+                .Select(r => _mapper.Map<ResourceResponse>(r)),
                 query.Page, query.Limit);
 
-        public async Task<ResourceInfo> Get(Guid id)
-            => _mapper.Map<ResourceInfo>(await _repository.Get(id)) ?? throw new ArgumentException("Resource not found");
+        public async Task<ResourceResponse> Get(Guid id)
+            => _mapper.Map<ResourceResponse>(await _repository.Get(id)) ?? throw new ArgumentException("Resource not found");
 
-        public async Task SaveResource(Resource source)
-            => await _repository.Add(source);
+        public async Task SaveResource(ResourceSaveRequest source)
+        {
+            var entity = _mapper.Map<Resource>(source);
+            await _repository.Add(entity);
+        }
 
         public async Task DeleteResource(Guid resourceId)
             => await _repository.Delete(await _repository.Get(resourceId) ?? throw new ArgumentException("Resource not found"));
--- MyPiggyBank.Core/Service/Interface/IResourcesService.cs
+++ MyPiggyBank.Core/Service/Interface/IResourcesService.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Threading.Tasks;
-using MyPiggyBank.Core.Protocol.Resource;
-using MyPiggyBank.Data.Model;
+﻿using MyPiggyBank.Core.Protocol.Resource.Requests;
+using MyPiggyBank.Core.Protocol.Resource.Responses;
 using MyPiggyBank.Data.Util;
+using System;
+using System.Threading.Tasks;
 
-namespace MyPiggyBank.Core.Service {
-    public interface IResourcesService {
-        Task SaveResource(Resource source);
+namespace MyPiggyBank.Core.Service
+{
+    public interface IResourcesService
+    {
         Task DeleteResource(Guid resourceId);
-        Task<ResourceInfo> Get(Guid id);
-        PagedList<ResourceInfo> GetResources(ResourcesQuery query);    
+        Task<ResourceResponse> Get(Guid id);
+        PagedList<ResourceResponse> GetResources(ResourceGetRequest query);
+        Task SaveResource(ResourceSaveRequest source);
     }
 }
