bc3c67d11bdfb0b0502beeffc117b5ff9b3b3af4
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:44:59 +0200

    OperationCategories Service and Interface updated to match changes made.

--- MyPiggyBank.Core/Service/Implementation/OperationCategoriesService.cs
+++ MyPiggyBank.Core/Service/Implementation/OperationCategoriesService.cs
@@ -1,15 +1,15 @@
 ﻿using AutoMapper;
-using MyPiggyBank.Core.Protocol.OperationCategories;
+using MyPiggyBank.Core.Protocol.OperationCategories.Requests;
+using MyPiggyBank.Core.Protocol.OperationCategories.Responses;
 using MyPiggyBank.Data.Model;
 using MyPiggyBank.Data.Repository;
 using MyPiggyBank.Data.Util;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace MyPiggyBank.Core.Service.Implementation {
+namespace MyPiggyBank.Core.Service.Implementation
+{
     public class OperationCategoriesService : IOperationCategoriesService
     {
         private readonly IOperationCategoriesRepository _repository;
@@ -21,18 +21,21 @@ namespace MyPiggyBank.Core.Service.Implementation {
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
-        public PagedList<OperationCategoryInfo> GetOperationCategories(OperationCategoriesQuery query)
-             => PagedList<OperationCategoryInfo>.ToPagedList(_repository.GetAll()
-                .Where(o => query.User == Guid.Empty || o.UserId == query.User)
+        public PagedList<OperationCategoriesResponse> GetOperationCategories(OperationCategoriesGetRequest query)
+             => PagedList<OperationCategoriesResponse>.ToPagedList(_repository.GetAll()
+                .Where(o => query.UserId == Guid.Empty || o.UserId == query.UserId)
                 .Where(o => o.Name == (query.Name ?? o.Name))
-                .Select(r => _mapper.Map<OperationCategoryInfo>(r)),
+                .Select(r => _mapper.Map<OperationCategoriesResponse>(r)),
                 query.Page, query.Limit);
 
-        public async Task<OperationCategoryInfo> Get(Guid id)
-            => _mapper.Map<OperationCategoryInfo>(await _repository.Get(id)) ?? throw new ArgumentException("Operation Category not found");
+        public async Task<OperationCategoriesResponse> Get(Guid id)
+            => _mapper.Map<OperationCategoriesResponse>(await _repository.Get(id)) ?? throw new ArgumentException("Operation Category not found");
 
-        public async Task SaveOperationCategory(OperationCategory source)
-            => await _repository.Add(source);
+        public async Task SaveOperationCategory(OperationCategoriesSaveRequest source)
+        {
+            var entity = _mapper.Map<OperationCategory>(source);
+            await _repository.Add(entity);
+        }
         public async Task DeleteOperationCategory(Guid operationCategoryId)
             => await _repository.Delete(await _repository.Get(operationCategoryId) ?? throw new ArgumentException("Operation Category not found"));
     }
