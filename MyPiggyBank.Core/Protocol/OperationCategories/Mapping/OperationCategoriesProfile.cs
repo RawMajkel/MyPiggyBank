34aeed734caf746b75868c5be3a2ec35c4811b03
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:36:34 +0200

    OperationCategoriesProfile added
    Single file for mapping

---
+++ MyPiggyBank.Core/Protocol/OperationCategories/Mapping/OperationCategoriesProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MyPiggyBank.Core.Protocol.OperationCategories.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyPiggyBank.Core.Protocol.OperationCategories.Mapping
+{
+    public class OperationCategoriesProfile : Profile
+    {
+        public OperationCategoriesProfile()
+        {
+            CreateMap<OperationCategoriesSaveRequest, Data.Model.OperationCategory>()
+                   .ForMember(dst => dst.Id, opt => opt.Condition(src => src.Id != Guid.Empty))
+                   .ForMember(d => d.User, opt => opt.Ignore())
+                   .ForMember(d => d.Operations, opt => opt.Ignore())
+                   .ForMember(d => d.CyclicOperations, opt => opt.Ignore());
+            CreateMap<Data.Model.OperationCategory, OperationCategoriesSaveRequest>();
+        }
+    }
+}
