689e958636c0514b77f34a3abfaff2af28f4ff85
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:47:34 +0200

    CyclicOperationProfile Test added
    Tests mapper and if creation of base Id

---
+++ MyPiggyBank.Test/CyclikOperation/CyclickOperationProfleTest.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MyPiggyBank.Core.Protocol.CyclicOperation.Mapping;
+using MyPiggyBank.Core.Protocol.CyclicOperation.Requests;
+using System;
+using Xunit;
+
+namespace MyPiggyBank.Test.CycliclOperations
+{
+    public class CyclickOperationProfleTest
+    {
+        private readonly MapperConfiguration _mapperConfiguration;
+
+        public CyclickOperationProfleTest()
+        {
+            _mapperConfiguration = new MapperConfiguration(c =>
+            {
+                c.AddProfile<CyclicOperationProfile>();
+            });
+        }
+
+        [Fact]
+        public void Profile_ShouldBeValid()
+        {
+            _mapperConfiguration.AssertConfigurationIsValid();
+        }
+        [Fact]
+        public void CyclicOperationIdMapping()
+        {
+            CyclicOperationSaveRequest test = new CyclicOperationSaveRequest { Id = Guid.NewGuid() };
+            var mapper = _mapperConfiguration.CreateMapper();
+            var entity = mapper.Map<Data.Model.CyclicOperation>(test);
+            Assert.True(entity.Id == test.Id);
+        }
+    }
+}
