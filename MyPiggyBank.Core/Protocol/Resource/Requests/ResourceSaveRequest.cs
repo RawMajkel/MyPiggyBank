5ac0d4976778249c284da61418910ac84edb4d58
Bartek <bartlomiejzon96@gmail.com>
2020-06-27 23:40:06 +0200

    ResourceRequests Added
    Get & Save Requests

---
+++ MyPiggyBank.Core/Protocol/Resource/Requests/ResourceSaveRequest.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyPiggyBank.Core.Protocol.Resource.Requests
+{
+    public class ResourceSaveRequest
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public string Currency { get; set; }
+    }
+}
