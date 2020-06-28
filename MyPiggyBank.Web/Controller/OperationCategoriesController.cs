9030dda98cf4cfb7fb1d651177daf04fce78d760
Kacper <kacpermatuszek2@gmail.com>
2020-06-28 21:35:57 +0200

    Added BaseController to catching exception. Refactored controllers.

--- MyPiggyBank.Web/Controller/OperationCategoriesController.cs
+++ MyPiggyBank.Web/Controller/OperationCategoriesController.cs
@@ -9,7 +9,7 @@ namespace MyPiggyBank.Web.Controller
 {
     [Route("api/v1/[controller]")]
     [ApiController]
-    public class OperationCategoriesController : ControllerBase
+    public class OperationCategoriesController : BaseController
     {
         private readonly IOperationCategoriesService _operationCategoriesService;
         public OperationCategoriesController(IOperationCategoriesService operationsService)
@@ -19,37 +19,27 @@ namespace MyPiggyBank.Web.Controller
 
         [HttpGet]
         public IActionResult Get([FromBody] OperationCategoriesGetRequest query)
-        {
-            var resources = _operationCategoriesService.GetOperationCategories(query);
-            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(resources.PagingData()));
-            return Ok(resources);
-        }
+            => ReturnBadRequestIfThrowError(() =>
+            {
+                var resources = _operationCategoriesService.GetOperationCategories(query);
+                Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(resources.PagingData()));
+                return resources;
+            });
 
         [HttpPost]
-        public void Post([FromBody] OperationCategoriesSaveRequest opc)
-            => _operationCategoriesService.SaveOperationCategory(opc);
+        public async Task<IActionResult> Post([FromBody] OperationCategoriesSaveRequest opc)
+            => await ReturnBadRequestIfThrowError(async() => await _operationCategoriesService.SaveOperationCategory(opc));
 
         [HttpGet("{id:guid}")]
         public async Task<IActionResult> Get(Guid id)
-        {
-            try
-            {
-                await _operationCategoriesService.Get(id);
-                return Ok();
-            }
-            catch (Exception e)
-            {
-
-                return BadRequest(e.Message);
-            }
-        }
+         => await ReturnBadRequestIfThrowError(async() => await _operationCategoriesService.Get(id));
 
         [HttpPut("{id:guid}")]
-        public void Put(Guid id, [FromBody] OperationCategoriesSaveRequest opc)
-            => _operationCategoriesService.SaveOperationCategory(opc);
+        public async Task<IActionResult> Put(Guid id, [FromBody] OperationCategoriesSaveRequest opc)
+            => await ReturnBadRequestIfThrowError(async() => await _operationCategoriesService.SaveOperationCategory(opc));
 
         [HttpDelete("{id:guid}")]
-        public void Delete(Guid id)
-            => _operationCategoriesService.DeleteOperationCategory(id);
+        public async Task<IActionResult> Delete(Guid id)
+            => await ReturnBadRequestIfThrowError(async() => await _operationCategoriesService.DeleteOperationCategory(id));
     }
 }
