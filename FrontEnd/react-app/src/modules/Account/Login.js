aacd91699911fcf3404da445bf09bf6bbae7843e
Michał Droździk <m.drozdzik97@gmail.com>
2020-07-03 17:02:56 +0200

    Added validation for Login component

--- FrontEnd/react-app/src/modules/Account/Login.js
+++ FrontEnd/react-app/src/modules/Account/Login.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import Authenticate from '../Global/Authenticate';
 
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 function Login() {
 
   const [email, setEmail] = useState([]);
   const [password, setPassword] = useState([]);
+
   const [isSubmitted, setIsSubmitted] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   function handleChange(event) {
     let id = event.target.getAttribute('id');
@@ -21,6 +28,23 @@ function Login() {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    let errors = [];
+
+    if(password.length <= 7 || !(/[A-Z]/.test(password)) || !(/[1-9]/.test(password)) || (/^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g.test(password))) {
+        errors.push("Hasło musi zawierać co najmniej 8 znaków, jedną wielką literę, jedną liczbę oraz jeden znak specjalny");
+    }
+
+    if(!validateEmail(email)) {
+        errors.push("Podaj poprawny adres e-mail");
+    }
+
+    setErrors(errors);
+
+    if(errors.length > 0) {
+        return;
+    }
+
     setIsSubmitted(true);
   }
 
@@ -39,6 +63,13 @@ function Login() {
                         <input placeholder="********" id="password" className="form-control" type="password" value={password} onChange={handleChange} />
                     </div>
                     <input className="button d-inline-flex align-items-center justify-content-start color-white" type="submit" value="Zaloguj" />
+                    <ul style={{ marginTop : '10px', color : 'red', listStyle : 'disc', listStylePosition : 'inside' }}>
+                        { 
+                            errors.map(function(element, index) {
+                                return <li className="error" key={index}>{element}</li>
+                            })
+                        }
+                    </ul>
                     { isSubmitted == true && <Authenticate email={email} password={password} /> }
                 </form>
             </div>
