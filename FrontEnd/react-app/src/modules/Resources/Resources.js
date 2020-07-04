ccf270b0ed3e597a5b007c24b327920496da1b2f
Michał Droździk <m.drozdzik97@gmail.com>
2020-07-04 21:17:11 +0200

    Created Resource.js and Resources.js components with tempData

--- FrontEnd/react-app/src/modules/Resources/Resources.js
+++ FrontEnd/react-app/src/modules/Resources/Resources.js
@@ -1,63 +1,37 @@
 import React from 'react';
+import Resource from '../Resources/Resource';
+import { v4 as uuidv4 } from 'uuid';
 
-function Resources() {
+function Resources({token}) {
+
+  const tempData = [
+    { "Id": uuidv4(), "Name": "Karta mBank", "Value": 3250.99, "Currency": "PLN" },
+    { "Id": uuidv4(), "Name": "Karta Revolut", "Value": 1090.31, "Currency": "PLN" },
+    { "Id": uuidv4(), "Name": "Gotówka", "Value": 300.03, "Currency": "PLN" }
+  ];
+
+  // return data
+  // public Guid Id { get; set; }
+  // public Guid UserId { get; set; }
+  // public string Name { get; set; }
+  // public decimal Value { get; set; }
+  // public string Currency { get; set; }
+
   return (
-  //   <div className="resources section">
-  //     <div className="container">
-  //         <span className="resources___heading heading d-block font-weight-600">Lista rachunków:</span>
-  //         <div className="resources__list">
-  //             <div className="resources__item d-flex align-items-center justify-content-between">
-  //                 <div className="resources__content d-flex align-items-center justify-content-start">
-  //                     <i className="fab fa-cc-visa resources__img"></i>
-  //                     <div className="resources__desc">
-  //                         <div className="resources__title">Karta mBank</div>
-  //                         <span className="resources__value font-weight-600 d-block">3092,55 zł</span>
-  //                     </div>
-  //                 </div>
-  //                 <div className="resource__close">
-  //                     <a className="resources__label d-flex align-items-center justify-content-start color-second" href="#">
-  //                         Zamknij 
-  //                         <i className="fas fa-times-circle resources__icon"></i>
-  //                     </a>
-  //                 </div>
-  //             </div>
-  //             <div className="resources__item d-flex align-items-center justify-content-between">
-  //                 <div className="resources__content d-flex align-items-center justify-content-start">
-  //                     <i className="fab fa-cc-mastercard resources__img"></i>
-  //                     <div className="resources__desc">
-  //                         <div className="resources__title">Karta Revolut</div>
-  //                         <span className="resources__value font-weight-600 d-block">1020,78 zł</span>
-  //                     </div>
-  //                 </div>
-  //                 <div className="resource__close">
-  //                     <a className="resources__label d-flex align-items-center justify-content-start color-second" href="#">
-  //                         Zamknij 
-  //                         <i className="fas fa-times-circle resources__icon"></i>
-  //                     </a>
-  //                 </div>
-  //             </div>
-  //             <div className="resources__item d-flex align-items-center justify-content-between">
-  //                 <div className="resources__content d-flex align-items-center justify-content-start">
-  //                     <i className="fas fa-money-bill-alt resources__img"></i>
-  //                     <div className="resources__desc">
-  //                         <div className="resources__title">Gotówka</div>
-  //                         <span className="resources__value font-weight-600 d-block">300,21 zł</span>
-  //                     </div>
-  //                 </div>
-  //                 <div className="resource__close">
-  //                     <a className="resources__label d-flex align-items-center justify-content-start color-second" href="#">
-  //                         Zamknij 
-  //                         <i className="fas fa-times-circle resources__icon"></i>
-  //                     </a>
-  //                 </div>
-  //             </div>
-  //         </div>
-  //         <a className="button d-inline-flex align-items-center justify-content-start">
-  //             <i className="fas fa-plus button__icon color-white"></i>
-  //             <span className="color-white">Dodaj rachunek</span>
-  //         </a>  
-  //     </div>
-  // </div>
+    <div className="resources section">
+      <div className="container">
+          <span className="resources___heading heading d-block font-weight-600">Lista rachunków:</span>
+          <div className="resources__list">
+            <div>
+                { tempData.map(function(el, index) { return <Resource id={el.Id} name={el.Name} value={el.Value} currency={el.Currency} key={index} /> }) }
+            </div>
+          </div>
+          <a href="/resources/add" className="button d-inline-flex align-items-center justify-content-start">
+              <i className="fas fa-plus button__icon color-white"></i>
+              <span className="color-white">Dodaj rachunek</span>
+          </a>  
+      </div>
+  </div>
   );
 }
 
