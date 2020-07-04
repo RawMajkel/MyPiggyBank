6f2f792fb234b50665fcb5d0c817ce5128b1f389
Michał Droździk <m.drozdzik97@gmail.com>
2020-07-04 22:29:51 +0200

    Prepared Add / Delete Resource components
     - will be finished after API fixes

---
+++ FrontEnd/react-app/src/modules/Resources/DeleteResource.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function DeleteResource({token}) {
+    
+
+    return (
+        <div className="resources section">
+            
+        </div>
+      );
+}
+
+export default DeleteResource;
