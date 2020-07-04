e1cbd7e3b22228d8e616ec091215113b57b4448a
Michał Droździk <m.drozdzik97@gmail.com>
2020-07-04 22:30:35 +0200

    Included new Operations component in Home

--- FrontEnd/react-app/src/modules/Home/Home.js
+++ FrontEnd/react-app/src/modules/Home/Home.js
@@ -1,96 +1,14 @@
 import React from 'react';
+import Operations from '../Operations/Operations';
 
-function Home() {
+function Home({token}) {
   return (
-    // <div className="latest section">
-    //   <div className="container">
-    //     <span className="latest__heading heading d-block font-weight-600">Ostatnie operacje:</span>
-    //     <div className="latest-group">
-    //       <div className="latest-group__date position-relative w-100 d-flex align-items-center justify-content-start">
-    //         <span className="latest-group__datelabel w-100 text-uppercase font-weight-600">Dzisiaj</span>
-    //       </div>
-    //       <div className="latest-group__list">
-    //         <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //           <i className="latest-item__icon fas fa-reply color-red"></i>
-    //           <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //             <div className="latest-item__title line-1 text-uppercase">Zakupy</div>
-    //             <span className="latest-item__value font-weight-500 w-100 text-right">79,31 zł</span>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="latest-group">
-    //       <div className="latest-group__date position-relative w-100 d-flex align-items-center justify-content-start">
-    //         <span className="latest-group__datelabel w-100 text-uppercase font-weight-600">13 maja</span>
-    //       </div>
-    //       <div className="latest-group__list">
-    //         <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //           <i className="latest-item__icon fas fa-share color-green"></i>
-    //           <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //             <div className="latest-item__title line-1 text-uppercase">Sprzedaż hantli na siłownię</div>
-    //             <span className="latest-item__value font-weight-500 w-100 text-right color-green">250,00 zł</span>
-    //           </div>
-    //         </div>
-    //           <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //             <i className="latest-item__icon fas fa-reply color-red"></i>
-    //             <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //               <div className="latest-item__title line-1 text-uppercase">Biedronka - zakupy</div>
-    //               <span className="latest-item__value font-weight-500 w-100 text-right">101,12 zł</span>
-    //             </div>
-    //           </div>
-    //           <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //             <i className="latest-item__icon fas fa-reply color-red"></i>
-    //             <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //               <div className="latest-item__title line-1 text-uppercase">Tankowanie samochodu</div>
-    //               <span className="latest-item__value font-weight-500 w-100 text-right">120,00 zł</span>
-    //             </div>
-    //           </div>
-    //         </div>
-    //     </div>
-    //     <div className="latest-group">
-    //       <div className="latest-group__date position-relative w-100 d-flex align-items-center justify-content-start">
-    //         <span className="latest-group__datelabel w-100 text-uppercase font-weight-600">10 maja</span>
-    //       </div>
-    //       <div className="latest-group__list">
-    //         <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //           <i className="latest-item__icon fas fa-reply color-red"></i>
-    //           <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //             <div className="latest-item__title line-1 text-uppercase">Gra komputerowa</div>
-    //             <span className="latest-item__value font-weight-500 w-100 text-right">101,12 zł</span>
-    //           </div>
-    //         </div>
-    //         <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //           <i className="latest-item__icon fas fa-reply color-red"></i>
-    //           <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //             <div className="latest-item__title line-1 text-uppercase">Skrzynka z narzędziami</div>
-    //             <span className="latest-item__value font-weight-500 w-100 text-right">231,85 zł</span>
-    //           </div>
-    //         </div>
-    //         <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //           <i className="latest-item__icon fas fa-reply color-red"></i>
-    //           <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //             <div className="latest-item__title line-1 text-uppercase">Naprawa kosiarki</div>
-    //             <span className="latest-item__value font-weight-500 w-100 text-right">75,99 zł</span>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="latest-group">
-    //       <div className="latest-group__date position-relative w-100 d-flex align-items-center justify-content-start">
-    //         <span className="latest-group__datelabel w-100 text-uppercase font-weight-600">9 maja</span>
-    //       </div>
-    //       <div className="latest-group__list">
-    //         <div className="latest-item d-flex align-items-center flex-nowrap justify-content-start">
-    //           <i className="latest-item__icon fas fa-reply color-red"></i>
-    //           <div className="latest-item__desc d-flex w-100 justify-content-between align-items-center">
-    //             <div className="latest-item__title line-1 text-uppercase">Spotify</div>
-    //             <span className="latest-item__value font-weight-500 w-100 text-right">19,99 zł</span>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    <div className="latest section">
+      <div className="container">
+        <span className="latest__heading heading d-block font-weight-600">Ostatnie operacje:</span>
+        <Operations token={token} limit={10} />
+      </div>
+    </div>
   );
 }
 
