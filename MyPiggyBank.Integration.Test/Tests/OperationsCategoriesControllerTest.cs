9614f96c4c632f7d3189ab054e3dbb8ee5f9a74a
Sinity <ezo.dev@gmail.com>
2020-08-30 23:39:36 +0200

    Implemented unit tests related to saving (POST) operation categories

--- MyPiggyBank.Integration.Test/Tests/OperationsCategoriesControllerTest.cs
+++ MyPiggyBank.Integration.Test/Tests/OperationsCategoriesControllerTest.cs
@@ -18,19 +18,31 @@ namespace MyPiggyBank.Integration.Test.Tests
         [Fact]
         public void CreateCyclicOperation_ShouldSuccessfullyPost()
         {
-
+            Assert.True(_apiClient.Post("/api/v1/OperationCategories/Save", SampleOperationCategory()).IsSuccessStatusCode);
         }
 
         [Fact]
         public void CreateCyclicOperation_ShouldSuccessfullyPostMultipleTimes()
         {
+            Assert.True(_apiClient.Post("/api/v1/OperationCategories/Save", SampleOperationCategory()).IsSuccessStatusCode);
 
+            var secondRes = SampleOperationCategory();
+            secondRes.Name = "AnotherCyclicOperation";
+            Assert.True(_apiClient.Post("/api/v1/OperationCategories/Save", secondRes).IsSuccessStatusCode);
         }
 
         [Fact]
         public void CreateCyclicOperation_ShouldPostCorrectData()
         {
+            Assert.True(_apiClient.Post("/api/v1/OperationCategories/Save", SampleOperationCategory()).IsSuccessStatusCode);
 
+            var getCyclicOperationResp = _apiClient.Get("/api/v1/OperationCategories/List?Name=TestOpCategory");
+            Assert.True(getCyclicOperationResp.IsSuccessStatusCode);
+
+            var ops = getCyclicOperationResp.Deserialize<IList<OperationCategoriesResponse>>();
+            Assert.Equal(1, ops.Count);
+            Assert.NotEqual(Guid.Empty, ops[0].Id);
+            Assert.Equal("TestOpCategory", ops[0].Name);
         }
 
         [Fact]
