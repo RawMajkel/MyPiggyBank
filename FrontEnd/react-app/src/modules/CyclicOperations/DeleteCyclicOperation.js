862c0420c7c3e18b2866bbdb793de14cee9f8b4c
Michał Droździk <m.drozdzik97@gmail.com>
2020-09-06 21:43:15 +0200

    Update DeleteCyclicOperation.js

--- FrontEnd/react-app/src/modules/CyclicOperations/DeleteCyclicOperation.js
+++ FrontEnd/react-app/src/modules/CyclicOperations/DeleteCyclicOperation.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useEffect } from 'react';
+import axios from 'axios';
 
 function DeleteCyclicOperation({token, id}) {
-  return (
-      <div>
-        DeleteCyclicOperation {id}
-      </div>
-  );
+
+  useEffect(() => {
+    (async () => {
+        const config = { headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": `Bearer ${token}` } };
+
+        const response = await axios.delete(`https://localhost:5001/api/v1/cyclicoperations/${id}`, config).then(response => {
+            if(response.status) {
+                window.location.href = "/cyclic";
+            }
+        }).catch(({response}) => {
+            console.log(response);
+        });
+    })();
+}, []);
+
+return (
+  <div className="resources section">
+    <div className="container">
+      <span className="heading d-block font-weight-600">Usuwam operację cykliczną</span>
+      <p>Proszę czekać</p>
+    </div>
+  </div>
+);
 }
 
 export default DeleteCyclicOperation;
