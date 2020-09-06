a6e928687c7cbd599aa92cb1d96897d94204cd4a
Michał Droździk <m.drozdzik97@gmail.com>
2020-09-06 19:44:21 +0200

    Update

--- MyPiggyBank.Core/Protocol/OperationCategories/Mapping/OperationCategoriesProfile.cs
+++ MyPiggyBank.Core/Protocol/OperationCategories/Mapping/OperationCategoriesProfile.cs
@@ -2,7 +2,6 @@
 using MyPiggyBank.Core.Protocol.OperationCategories.Requests;
 using MyPiggyBank.Core.Protocol.OperationCategories.Responses;
 using System;
-using MyPiggyBank.Core.Protocol.OperationCategories.Responses;
 
 namespace MyPiggyBank.Core.Protocol.OperationCategories.Mapping
 {
