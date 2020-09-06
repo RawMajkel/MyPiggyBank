f6fe7039352463ca7f488a9c62c35d37ef98db33
Michał Droździk <m.drozdzik97@gmail.com>
2020-09-06 21:41:55 +0200

    Updated DeleteReseource.js

--- FrontEnd/react-app/src/modules/Resources/DeleteResource.js
+++ FrontEnd/react-app/src/modules/Resources/DeleteResource.js
@@ -1,14 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import axios from 'axios';
 
-function DeleteResource({token}) {
+function DeleteResource({id, token}) {
     
+  useEffect(() => {
+    (async () => {
+        const config = { headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": `Bearer ${token}` } };
 
-    return (
-        <div className="resources section">
-            
-        </div>
-      );
+        const response = await axios.delete(`https://localhost:5001/api/v1/resources/${id}`, config).then(response => {
+            if(response.status) {
+                window.location.href = "/resources";
+            }
+        }).catch(({response}) => {
+            console.log(response);
+        });
+    })();
+}, []);
+
+  return (
+    <div className="resources section">
+      <div className="container">
+        <span className="heading d-block font-weight-600">Usuwam rachunek</span>
+        <p>Proszę czekać</p>
+      </div>
+    </div>
+  );
 }
 
 export default DeleteResource;
