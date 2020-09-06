fbea76719389a11a926d0b8daf3a18b403e8cd13
Michał Droździk <m.drozdzik97@gmail.com>
2020-09-06 21:41:23 +0200

    Created DeleteCategory module

--- FrontEnd/react-app/src/modules/Categories/DeleteCategory.js
+++ FrontEnd/react-app/src/modules/Categories/DeleteCategory.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
 
 function DeleteCategory({id, token}) {
+
+  useEffect(() => {
+      (async () => {
+          const config = { headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": `Bearer ${token}` } };
+
+          const response = await axios.delete(`https://localhost:5001/api/v1/operationcategories/${id}`, config).then(response => {
+              if(response.status) {
+                  window.location.href = "/categories";
+              }
+          }).catch(({response}) => {
+              console.log(response);
+          });
+      })();
+  }, []);
+
   return (
-    <div>
-        DeleteCategory {id}
+    <div className="resources section">
+      <div className="container">
+        <span className="heading d-block font-weight-600">Usuwam kategorię</span>
+        <p>Proszę czekać</p>
+      </div>
     </div>
   );
 }
